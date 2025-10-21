da8746f4cd07967657df34f38bfb12da939b5f99
Feuille Ciseaux <84070376+feuilleciseau@users.noreply.github.com>
2025-10-21 04:39:15 +0200

    Add unique url

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
 from sys import argv
-from utils.yt_downloader import download_audio_single, download_audio_playlist
+import json
+from utils.yt_downloader import download_audio_single, download_audio
 from utils.cli import CLI
 from utils.gui import GUI
 from utils.help import Help
 
-def main():
-    if len(argv) == 1:
-        Help.show()
 
-    elif len(argv) == 2:
-        cmd = argv[1]
+def get_config():
+    try:
+        with open("config.json", 'r', encoding="utf-8") as file:
+            config = json.load(file)
+            return config
 
-        if cmd == "cli":
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"ERROR: Fail to read the config file")
+        return {}
+    
+def download_from_txt(urls_file):
+    try:
+        with open(urls_file, "r") as file:
+            urls = [line.strip() for line in file if line.strip()]
+            for url in urls:
+                download_audio_single(url, config.get("output_folder"))
+
+    except FileNotFoundError:
+        print(f"{urls_file} not found !")
+
+def main(config):
+    if len(argv) == 2:
+
+        if argv[1] == "cli":
             cli = CLI()
             cli.run()
 
-        elif cmd == "gui":
+        elif argv[1] == "gui":
             gui = GUI()
             gui.run()
 
-        elif cmd == "help":
+        elif argv[1] == "help":
             Help.show()
 
-    elif len(argv) == 3:
-        cmd = argv[1]
-        input_arg = argv[2]
+        elif argv[1].startswith("https://"):
+            download_audio(argv[1], config.get("output_folder"))
 
-        if cmd == "s":
-            if input_arg.endswith(".txt"):
-                try:
-                    with open(input_arg, "r") as file:
-                        urls = [line.strip() for line in file if line.strip()]
-                        for url in urls:
-                            download_audio_single(url)
+        elif argv[1].endswith(".txt"):
+            download_from_txt(argv[1], config.get("output_folder"))
 
-                except FileNotFoundError:
-                    print(f"{input_arg} not found !")
+        else:
+            Help.show()
 
-        elif cmd == "p":
-            download_audio_playlist(input_arg)
-
+    else:
+        Help.show()
 
 if __name__ == "__main__":
-    main()
+    config = get_config()
+    main(config)
